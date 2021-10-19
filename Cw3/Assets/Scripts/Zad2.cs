07334c131f7b98199aaee40ad88f017b844c9e8b
Frafro122 <plo9999@interia.pl>
2021-10-19 19:05:29 +0200

    Zad3

---
+++ Cw3/Assets/Scripts/Zad2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zad2 : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody rb;
+    Vector3 startPosition;
+    Vector3 endPosition;
+    int turn = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPosition = rb.position;
+        endPosition = startPosition + new Vector3(10, 0, 0);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(rb.position, endPosition) >= 0.1f)
+        {
+            //Debug.Log(Vector3.Distance(startPosition, endPosition));
+            Vector3 velocity = new Vector3(1, 0, 0);
+            velocity = velocity.normalized * turn * speed * Time.deltaTime;
+            rb.MovePosition(transform.position + velocity);
+        }
+        else
+        {
+            turn = -turn;
+            endPosition = rb.position + new Vector3(10 * turn, 0, 0);
+        }
+    }
+}
---
+++ Cw3/Assets/Scripts/Zad4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zad4 : MonoBehaviour
+{
+    public float speed = 10.0f;
+    GameObject[] enemies;
+    GameObject player;
+    void Start()
+    {
+        
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(xDirection, 0, zDirection);
+        player.transform.position += moveDirection * speed;
+        foreach(var enemy in enemies)
+        {
+            float distanse = Vector3.Distance(player.transform.position, enemy.transform.position);
+            
+            if (distanse <= 1.6f)
+                Debug.Log("You hugged enemy");
+        }
+    }
+}
