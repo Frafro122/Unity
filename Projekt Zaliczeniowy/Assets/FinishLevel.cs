3ec7b118e0889cc84ca66ffcb1a03ae4c1060389
Frafro122 <plo9999@interia.pl>
2021-12-13 19:41:35 +0100

    Bug

---
+++ Projekt Zaliczeniowy/Assets/FinishLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel : MonoBehaviour
+{
+    private Player player;
+    private Animator animator;
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x >= transform.position.x)
+        {
+            player.playerbody.simulated = false;
+            player.transform.position = new Vector3(187.7f, 5.5f, player.transform.position.z);
+            player.animator.SetTrigger("isFinished");
+            animator.SetTrigger("isFinished");
+        }
+    }
+}
